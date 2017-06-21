4bc650175dfd81ac6f401e2394e5f83970b266d8
Jamie Bull <jamie.bull@oco-carbon.com>
2017-06-21 18:41:20 +0200

    Handle "yesterday" and "tomorrow" and lower & strip input text

--- dehumanize/dehumanize.py
+++ dehumanize/dehumanize.py
@@ -3,12 +3,18 @@ import re
 
 
 def naturaltime(text, now=None):
-    """Convert a django naturaltime string to a datetime object."""
+    """Convert a naturaltime string to a datetime object."""
+    text = text.lower().strip()
     if not now:
         now = datetime.now()
 
     if text == 'now':
         return now
+    if text == 'yesterday':
+        return now - timedelta(days=1)
+    if text == 'tomorrow':
+        return now + timedelta(days=1)
+
     if "ago" in text:
         multiplier = -1
     elif "from now" in text:
@@ -19,11 +25,20 @@ def naturaltime(text, now=None):
     text = text.replace('an ', '1 ')
     text = text.replace('a ', '1 ')
 
+    years = get_first(r'(\d*) year', text)
+    months = get_first(r'(\d*) month', text)
+    weeks = get_first(r'(\d*) week', text)
     days = get_first(r'(\d*) day', text)
+    days = days + weeks * 7 + months * 30 + years * 365
+
     hours = get_first(r'(\d*) hour', text)
     minutes = get_first(r'(\d*) minute', text)
     seconds = get_first(r'(\d*) second', text)
-    delta = timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds)
+    delta = timedelta(
+        days=days,
+        hours=hours,
+        minutes=minutes,
+        seconds=seconds)
     delta *= multiplier
     return now + delta
 
